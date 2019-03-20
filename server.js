46f29cc779f906f615751b6c7264bb9bc8a5b1ba
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-20 13:24:47 -0400

    Creates custom middleware 'bouncer' and includes a server.use() call for it

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+// custom middleware - just a function
+// ***something that blocks every request
+function bouncer(req, res, next) {
+  res.status(404).json("These are not the droids you're looking for");
+}
+
+
 // middleware
-server.use(express.json());
-server.use(helmet());
+server.use(bouncer); // <-- calls custom middleware
+server.use(express.json()); // built-in; no need to install
+server.use(helmet()); // third-party, needs to be added as a dependency first (yarn add)
 
 // routing
 server.use('/api/hubs', hubsRouter);
