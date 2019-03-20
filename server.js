52dbf11ebcca646f3eb679d61d91dc311df0915c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-20 13:48:10 -0400

    Creates a new custom middleware called 'moodyGateKeeper', adds it to list of middlware with 'server.use()' statement, and discusses the difference between .json() and .send()

--- server.js
+++ server.js
@@ -8,12 +8,24 @@ const server = express();
 // custom middleware - just a function that gets access to req, res, and next.
 // ***something that blocks every request
 function bouncer(req, res, next) {
-  res.status(404).json("These are not the droids you're looking for");
+  return res.status(404).json("These are not the droids you're looking for");
+  next(); // <== this will not execute because the 'return' statement ensures that it won't run
 }
 
 function teamNamer(req, res, next) {
   req.team = 'Web XVII'; // LOOK! Middleware can modify the request!
-  next(); // go ahead and execute the next middleware/route handler
+  next(); // this says 'go ahead and execute the next middleware/route handler'
+}
+
+// this custom middleware returns 403 status code and 'you shall not pass!' when clock
+// clock seconds are multiples of 3, other times call next();
+function moodyGateKeeper(req, res, next) {
+  const seconds = new Date().getSeconds();
+  if (seconds % 3 === 0) {
+    res.status(403).send("You shall not pass!");
+  } else {
+    next();
+  }
 }
 
 // middleware
@@ -22,6 +34,7 @@ server.use(express.json()); // built-in; no need to install
 server.use(helmet()); // third-party, needs to be added as a dependency first (yarn add)
 // server.use(bouncer); // <-- calls custom middleware
 server.use(teamNamer);
+server.use(moodyGateKeeper);
 
 
 // routing
