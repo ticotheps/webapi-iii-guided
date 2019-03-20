f3319b2473ab161b423b5becba6a0a96c73a0a43
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-20 13:33:15 -0400

    Creates new custom middleware function called 'teamNamer' and adds it to the middleware list with 'server.use()' statement

--- server.js
+++ server.js
@@ -5,25 +5,33 @@ const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-// custom middleware - just a function
+// custom middleware - just a function that gets access to req, res, and next.
 // ***something that blocks every request
 function bouncer(req, res, next) {
   res.status(404).json("These are not the droids you're looking for");
 }
 
+function teamNamer(req, res, next) {
+  req.team = 'Web XVII'; // LOOK! Middleware can modify the request!
+  next(); // go ahead and execute the next middleware/route handler
+}
 
 // middleware
-server.use(bouncer); // <-- calls custom middleware
+
 server.use(express.json()); // built-in; no need to install
 server.use(helmet()); // third-party, needs to be added as a dependency first (yarn add)
+// server.use(bouncer); // <-- calls custom middleware
+server.use(teamNamer);
+
 
 // routing
+// route handlers ARE basically middleware
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res, next) => {
   res.send(`
     <h2>Lambda Hubs API</h2>
-    <p>Welcome to the Lambda Hubs API</p>
+    <p>Welcome ${req.team} to the Lambda Hubs API</p>
     `);
 });
 
